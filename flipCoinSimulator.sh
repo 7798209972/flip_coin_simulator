0bca36db9bbf41cb7847d578757a95ff816eaf6d
7798209972 <patlesachin1@gmail.com>
2020-09-25 14:07:19 +0530

    Flipping a Coin to Display Head or Tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,3 +4,22 @@
 echo "Welcome to Flip Coin Simulation Problem";
 echo "=======================================";
 printf "\n";
+
+#Declaring Counter Variable
+head_count=0;
+tail_count=0;
+
+
+#Using Random Function for Hetting head or Tail
+flip=$(( RANDOM %2+1 ));
+
+#Checking Head or Tail
+
+if [ $flip -eq 1 ]
+then
+     echo "Head " ;
+     (( head_count ++ ));
+else
+     echo  "Tail " ;
+     (( tail_count ++ ));
+fi
