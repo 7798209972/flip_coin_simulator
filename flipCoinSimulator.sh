7b6ea6eaf50f1074c71832ebb0e6204a467acceb
7798209972 <patlesachin1@gmail.com>
2020-09-25 14:31:40 +0530

    Multiple times Flipping and Displaying No of Head and Tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -8,16 +8,31 @@ printf "\n";
 #Declaring Counter Variable
 head_count=0;
 tail_count=0;
+count=0;
+
+while [[ $(( count )) -lt 11 ]]
+do
+	#Random function for getting Head or Tail
+        flip=$(( RANDOM %2+1 ));
+
+	#Checking Head or Tail
+
+        if [ $flip -eq 1 ]
+        then
+                echo "Head " ;
+		#increasing head counter
+                (( head_count ++ ));
+        else
+                echo  "Tail " ;
+		#increasing tail counter
+                (( tail_count ++ ));
+        fi
+
+	#increasing loop counter
+	(( count ++ ));
+done
 
 
-#Using Random Function for Hetting head or Tail
-flip=$(( RANDOM %2+1 ));
-
-#Checking Head or Tail
-
-if [ $flip -eq 1 ]
-then
-     echo "Head " ;
-else
-     echo  "Tail " ;
-fi
+#Displaying number of times Head or Tail has won
+printf "\n";
+echo "$head_count times Head & $tail_count times Tail"
